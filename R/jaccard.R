905622681f345c18040d0415efb41eb646de9298
AndresALEPH <32395760+AndresALEPH@users.noreply.github.com>
2018-02-07 12:29:38 -0600

    Update jaccard.R

--- R/jaccard.R
+++ R/jaccard.R
@@ -1,3 +1,10 @@
+#'Función para calcular el coeficiente Jaccard.
+#'
+#'Esta función calcula el coeficiente Jaccard de las variables de una tabla.
+#'@param x La tabla con los valores booleanos de cada variable
+#'@export
+#'@keywords jaccard
+
 jaccard <- function(x) {
   m <- matrix(NA, nrow=ncol(x),ncol=ncol(x),dimnames=list(colnames(x),colnames(x)))
   jacc <- as.data.frame(m)
